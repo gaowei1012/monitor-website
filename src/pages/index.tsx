4c0f65de0b3459fa9ba32bd2e1a0cc49c4796068
gaowei1012 <gyb2020018@163.com>
2025-04-08 19:33:16 +0800

    feat: 初始化项目

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+/*
+ * @Author: gaowei1012 gyb2020018@163.com
+ * @Date: 2025-04-08 19:20:30
+ * @LastEditors: gaowei1012 gyb2020018@163.com
+ * @LastEditTime: 2025-04-08 19:31:15
+ * @FilePath: /monitor-website/src/pages/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): ReactNode {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      {/* <HomepageHeader /> */}
+      {/* <main>
+        <HomepageFeatures />
+      </main> */}
+    </Layout>
+  );
+}
---
+++ src/pages/markdown-page.md
@@ -0,0 +1,18 @@
+<!--
+ * @Author: gaowei1012 gyb2020018@163.com
+ * @Date: 2025-04-08 19:20:30
+ * @LastEditors: gaowei1012 gyb2020018@163.com
+ * @LastEditTime: 2025-04-08 19:26:56
+ * @FilePath: /monitor-website/src/pages/markdown-page.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+---
+title: Markdown page example
+---
+
+# Markdown page example
+
+You don't need React to write simple standalone pages.
+
+
+哈哈哈
